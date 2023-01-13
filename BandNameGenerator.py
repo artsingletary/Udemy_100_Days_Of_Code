05d399ab482ac7ea7fe43770294250280d198bff
Art Singletary <artsingletary@yahoo.com>
2023-01-12 20:02:00 -0500

    First Commit

---
+++ BandNameGenerator.py
@@ -0,0 +1,5 @@
+#Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in? \n")
+pet_name = input("What's your Pet's name? \n")
+print(f" Your band name could be {city} {pet_name}")
---
+++ README.md
@@ -0,0 +1,1 @@
+# Udemy_100_Days
