54cbbefef9dd6591b34f95d0c02a4cefbdf0d9bc
Art Singletary <artsingletary@yahoo.com>
2023-02-05 14:47:24 -0500

    Added LoveCalculator.py

---
+++ LoveCalculator.py
@@ -0,0 +1,24 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+combined_string = name1 + name2
+lowercase_string = combined_string.lower()
+
+
+t = combined_string.count('t')
+r = combined_string.count('r')
+u = combined_string.count('u')
+e = combined_string.count('e')
+
+l = combined_string.count('l')
+o = combined_string.count('o')
+v = combined_string.count('v')
+e = combined_string.count('e')
+
+
+
+print(f"The letter e occurs {e} times")
+
