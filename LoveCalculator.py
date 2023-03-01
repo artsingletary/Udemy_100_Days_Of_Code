e07802a45668027bb4e8d8dc2d7c7da4cc24e90d
Art Singletary <artsingletary@yahoo.com>
2023-03-01 07:19:36 -0500

    Added if elif else logic

--- LoveCalculator.py
+++ LoveCalculator.py
@@ -22,7 +22,12 @@ e = combined_string.count('e')
 
 love = l + o + v + e
 
-love_score = str(true) + str(love)
+love_score = int(str(true) + str(love))
 
-print(f"Your total love score is {love_score}")
 
+if (love_score < 10) or (love_score > 90):
+     print(f"Your total love score is {love_score}, you go together like coke and mentos.")
+elif (love_score >= 40) and (love_score <= 50):
+     print(f"Your total love score is {love_score}, you are alright together.")
+else: 
+     print(f"Your total love score is {love_score}.")
